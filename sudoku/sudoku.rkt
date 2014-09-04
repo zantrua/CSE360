e42cee380b3d650756e81fec4a078e068a19656c
zantrua <zantrua@gmail.com>
2014-09-04 15:04:52 -0700

    Minor changes to fix printer for wider boards Code cleanup

--- sudoku/sudoku.rkt
+++ sudoku/sudoku.rkt
@@ -20,9 +20,27 @@
   (list-ref (list-ref p y) x))
 
 (define (puzzle-print p)
-  (define width (puzzle-width p))
-  (for ([r width])
-    (display (format "~%+---+---+---+---+---+---+---+---+---+~%|"))
-    (for ([c width])
-      (display (format " ~A |" (puzzle-ref p r c)))))
-  (display (format "~%+---+---+---+---+---+---+---+---+---+~%~%")))
+  (let* ([width (puzzle-width p)]
+         [display-line (λ (last-line) (display (format "~%~a+~%~a"
+                                                       (foldl string-append "" (build-list width (λ (n) "+---")))
+                                                       (if last-line
+                                                           ""
+                                                           "|"))))])
+    (for ([y width])
+      (display-line #f)
+      (for ([x width])
+        (display (format " ~a |" (puzzle-ref p x y)))))
+    (display-line #t)))
+
+; Test cases
+
+(define test-board-solved
+  '((5 3 4 6 7 8 9 1 2)
+    (6 7 2 1 9 5 3 4 8)
+    (1 9 8 3 4 2 5 6 7)
+    (8 5 9 7 6 1 4 2 3)
+    (4 2 6 8 5 3 7 9 1)
+    (7 1 3 9 2 4 8 5 6)
+    (9 6 1 5 3 7 2 8 4)
+    (2 8 7 4 1 9 6 3 5)
+    (3 4 5 2 8 6 1 7 9)))
