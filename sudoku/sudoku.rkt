b33451895ee60a29db5ad8ce4ea9cd3845df2191
zantrua <zantrua@gmail.com>
2014-09-04 13:42:48 -0700

    Added a print method for puzzles

--- sudoku/sudoku.rkt
+++ sudoku/sudoku.rkt
@@ -13,3 +13,16 @@
   (build-list width
               (λ (y) (build-list width
                                  (λ (x) 0)))))
+(define (puzzle-width p)
+  (length p))
+
+(define (puzzle-ref p x y)
+  (list-ref (list-ref p y) x))
+
+(define (puzzle-print p)
+  (define width (puzzle-width p))
+  (for ([r width])
+    (display (format "~%+---+---+---+---+---+---+---+---+---+~%|"))
+    (for ([c width])
+      (display (format " ~A |" (puzzle-ref p r c)))))
+  (display (format "~%+---+---+---+---+---+---+---+---+---+~%~%")))
